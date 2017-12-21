11273e1704cf39c056acb756cdbe73207b5ca7ab
Eric Tanaka <cirept612@gmail.com>
2017-12-21 15:07:44 -0800

    creating initial release

---
+++ assets/js/meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Autofill Tool
+// @namespace www.cobaltgroup.com
+// @version 1.0
+// @run-at document-end
+// @downloadURL https://raw.githubusercontent.com/cirept/autofillReplacer/master/assets/js/meta.js
+// @include http://websites.cobalt.com/wsm/editSite.do*
+// @include http://websites.cobalt.com/wsm/cms.do*
+// @require https://code.jquery.com/jquery-3.2.1.js
+// @require https://cdn.jsdelivr.net/sortable/latest/Sortable.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/light.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/solid.min.js
+// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/fontawesome.min.js
+// @require https://rawgit.com/cirept/WSMupgrades/1.0/js/autofillTag.js
+// @author cire.pt
+// ==/UserScript==
