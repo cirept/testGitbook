02b3f28f330a116eeac962528a057f7c6af6d7ee
Eric Tanaka <cirept612@gmail.com>
2017-12-21 15:11:17 -0800

    version 1 release updates
    - copied over more dependency files
    - updated meta tage to new file locations

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -8,9 +8,9 @@
 // @include http://websites.cobalt.com/wsm/cms.do*
 // @require https://code.jquery.com/jquery-3.2.1.js
 // @require https://cdn.jsdelivr.net/sortable/latest/Sortable.min.js
-// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/light.min.js
-// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/packs/solid.min.js
-// @require https://cdn.rawgit.com/cirept/WSMupgrades/master/js/fontawesome.min.js
-// @require https://rawgit.com/cirept/WSMupgrades/1.0/js/autofillTag.js
+// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/js/packs/light.min.js
+// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/js/packs/solid.min.js
+// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/js/fontawesome.min.js
+// @require https://rawgit.com/cirept/autofillReplacer/1.0/js/autofillTag.js
 // @author cire.pt
 // ==/UserScript==
