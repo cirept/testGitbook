0c297e02101b0c76de657a6d224441834d7ef8a2
Eric Tanaka <cirept612@gmail.com>
2018-01-08 12:43:29 -0800

    Updated FONT AWESOME integration
    Removed calls to separate FA js files.  Used combined js file instead.

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,17 +1,18 @@
 // ==UserScript==
-// @name Autofill Tool 1.1.1
+// @name Autofill Tool 1.2.0-beta
 // @namespace www.cobaltgroup.com
-// @version 1.1.1
+// @version 1.2.0-beta
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/cirept/autofillReplacer/master/assets/js/meta.js
 // @include http://websites.cobalt.com/wsm/editSite.do*
 // @include http://websites.cobalt.com/wsm/cms.do*
 // @require https://code.jquery.com/jquery-3.2.1.js
 // @require https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js
-// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/packs/regular.min.js
-// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/packs/light.min.js
-// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/packs/solid.min.js
-// @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/fontawesome.min.js
-// @require https://rawgit.com/cirept/autofillReplacer/1.1.1/assets/js/autofillTag.js
+// @require https://cdn.rawgit.com/cirept/autofillReplacer/1.2.0-beta/assets/js/fontawesome-all.min.js
+// @require https://rawgit.com/cirept/autofillReplacer/1.2.0-beta/assets/js/meta.js
+// @grant GM_info
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_listValues
 // @author cire.pt
 // ==/UserScript==
