cbdaf05bb70956b005136563c876ca7c8f0cc2c3
Eric Tanaka <cirept612@gmail.com>
2018-07-09 15:51:50 -0700

    created develop user script

--- assets/js/develop.meta.user.js
+++ assets/js/develop.meta.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name Autofill Tool 1.1.1
+// @name Autofill Tool 1.1.5-beta2
 // @namespace www.cobaltgroup.com
-// @version 1.1.1
+// @version 1.1.5-beta2
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/cirept/autofillReplacer/master/assets/js/meta.js
 // @include http://websites.cobalt.com/wsm/editSite.do*
 // @include http://websites.cobalt.com/wsm/cms.do*
-// @require https://code.jquery.com/jquery-3.2.1.js
-// @require https://cdn.jsdelivr.net/sortable/latest/Sortable.min.js
+// @require https://code.jquery.com/jquery-3.3.1.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js
+// @require https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js
 // @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/packs/light.min.js
 // @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/packs/solid.min.js
 // @require https://cdn.rawgit.com/cirept/autofillReplacer/master/assets/js/fontawesome.min.js
-// @require https://rawgit.com/cirept/autofillReplacer/1.1.1/assets/js/autofillTag.js
+// @require https://rawgit.com/cirept/autofillReplacer/1.1.5-beta2/assets/js/autofillTag.js
 // @author cire.pt
 // ==/UserScript==
